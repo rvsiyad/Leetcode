ca64f884ba4aea6bbdb9b1b21c6380f74155bcc6
Roble <roblesiyad123@gmail.com>
2024-04-28 14:03:56 +0100

    Initializing and traversing over an array

---
+++ arrays/tutorial/static_arrays.py
@@ -0,0 +1,30 @@
+class StaticArrays:
+  #Creating an array in python:
+  myArrayNum = [1,2,3,4,5]
+  myArrayString = ['a','b','c','d','e']
+
+  # Arrays are accessed via an index and can be printed using the print() method:
+  def print_index(array):
+    print(array[0]) # This will print 1.
+
+  print_index(myArrayNum)
+  print('-------------------------')
+
+  # Arrays can be traversed over using a For loop:
+  def print_via_for_loop(array):
+    for i in range (len(array)):
+      print(array[i])
+
+  print_via_for_loop(myArrayString)
+  print('-------------------------')
+
+  # Or a While loop:
+  def print_via_while_loop(array):
+    i = 0
+    while(i < len(array)):
+      print(array[i])
+      i = i + 1
+
+  print_via_for_loop(myArrayNum)
+  print('-------------------------')
+
