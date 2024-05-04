c69df88ccd2518a104a2f6b95f78d219d03e717e
Roble <roblesiyad123@gmail.com>
2024-05-04 15:03:37 +0100

    Sliding Window fixed

---
+++ arrays/algorithims/sliding_window_fixed_size.py
@@ -0,0 +1,8 @@
+class SlidingWindowFixedSize:
+  # Sliding window for a fixed window size, works by having two pointers, K length apart.
+
+  # Here is an example question:
+  # Q: Given an array, return true if there are two elements within a window of size
+  # that are equal.
+  def containsDuplicates(nums, k):
+    print('hi')
