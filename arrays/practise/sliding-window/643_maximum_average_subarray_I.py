5fdcf762a18170c63bd46b2ffad877b061c871b1
Roble <roblesiyad123@gmail.com>
2024-05-15 09:33:51 +0100

    The solution

--- arrays/practise/sliding-window/643_maximum_average_subarray_I.py
+++ arrays/practise/sliding-window/643_maximum_average_subarray_I.py
@@ -16,3 +16,16 @@ class MaximumAverageSubarrayI:
   Output: 5.00000
 
   """
+  def findMaxAverage(nums, k):
+    currentSum = sum(nums[:k]) # First we initialise the currentSum to the sum of first k elements in array
+    maxSum = currentSum # Assign a maxSum value to the currentSum
+
+    L = 0 # Create left pointer
+
+    for R in range(k, len(nums)): # Loop from k to the end of array
+      currentSum += nums[R] # Add value at the right pointer
+      currentSum -= nums[L] # Subtract value from the left pointer
+
+      maxSum = max(maxSum, currentSum) # Take the max between the currentSum and the maxSum
+
+    return maxSum / k # Return the average
