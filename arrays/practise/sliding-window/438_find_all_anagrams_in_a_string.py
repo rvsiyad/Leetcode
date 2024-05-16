70ad06e41ad35c1c2716b2c525bb099d8dd5554f
Roble <roblesiyad123@gmail.com>
2024-05-16 12:41:12 +0100

    The solution

--- arrays/practise/sliding-window/438_find_all_anagrams_in_a_string.py
+++ arrays/practise/sliding-window/438_find_all_anagrams_in_a_string.py
@@ -14,3 +14,32 @@ class FindAllAnagramsInString:
   The substring with start index = 0 is "cba", which is an anagram of "abc".
   The substring with start index = 6 is "bac", which is an anagram of "abc".
   """
+
+  def findAnagrams(s, p):
+    if len(p) > len(s): return []
+
+    pCount, sCount = {}, {}
+
+    for i in range(len(p)):
+      pCount[p[i]] = 1 + pCount.get(p[i], 0)
+      sCount[s[i]] = 1 + sCount.get(s[i], 0)
+
+    res = [0] if sCount == pCount else []
+
+    L = 0
+
+    for R in range(len(p), len(s)):
+      sCount[s[R]] = 1 + sCount.get(s[R], 0)
+      sCount[s[L]] -= 1
+
+      if sCount[s[L]] == 0:
+        sCount.pop(s[L])
+
+      L += 1
+
+      if sCount == pCount:
+        res.append(L)
+
+    return res
+
+  print(findAnagrams('qwertyqweerqweeqweq','qew'))
