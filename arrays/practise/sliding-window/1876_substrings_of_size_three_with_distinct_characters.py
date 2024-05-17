8d32e783d256aa87029f17b11be5c91567f2f3b6
Roble <roblesiyad123@gmail.com>
2024-05-17 18:39:03 +0100

    The solution

--- arrays/practise/sliding-window/1876_substrings_of_size_three_with_distinct_characters.py
+++ arrays/practise/sliding-window/1876_substrings_of_size_three_with_distinct_characters.py
@@ -14,3 +14,36 @@ class SubstringsOfSizeThree:
   Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz".
   The only good substring of length 3 is "xyz".
   """
+
+  def countGoodSubstrings(s):
+    if len(s) < 3: return 0
+
+    count = 0
+    sCount = {}
+
+    # check first 3 characters of the string
+    # Loop through len of 3 of s,
+    for i in range(3):
+      sCount[s[i]] = 1 + sCount.get(s[i], 0) # add to a hashmap, setting value to 0 if first time or + 1 if already present
+
+    # If mapLength == 3, count +=1
+    if len(sCount) == 3: count += 1
+    # Left pointer set to 0
+    L = 0
+    # Loop from 3, to len of s, R pointer
+    for R in range(3, len(s)):
+      sCount[s[R]] + 1 + sCount.get(s[R], 0)  # Increment value on the right
+      sCount[s[L]] -= 1 # Decrement value on the left
+
+      # Remove left pointer if 0:
+      if sCount[s[L]] == 0:
+          sCount.pop(s[L])
+
+    # If mapLength == 3, count +=1
+      if len(sCount) == 3: count += 1
+
+    # Increment L pointer
+      L += 1
+
+    # return Count
+    return count
