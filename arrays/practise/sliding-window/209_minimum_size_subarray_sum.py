27f1193e6f08b2af72f5457b060edc2f49553463
Roble <roblesiyad123@gmail.com>
2024-05-18 15:29:02 +0100

    The solution

--- arrays/practise/sliding-window/209_minimum_size_subarray_sum.py
+++ arrays/practise/sliding-window/209_minimum_size_subarray_sum.py
@@ -10,3 +10,20 @@ class minSubArrayLen:
   Output: 2
   Explanation: The subarray [4,3] has the minimal length under the problem constraint.
   """
+
+  def minSubArrayLen(target, nums):
+    minLength = float("inf") # Initialise a minLength variable.
+    total = 0 # Keep track of a total sum.
+
+    L = 0 # Left pointer
+
+    for R in range(len(nums)): # Loop through length of nums
+      total += nums[R] # Add value at R pointer to total
+
+      while total >= target: # While the total is greater or  equal to the target.
+        total -= nums[L] # Subtract the L pointer from the total
+        minLength = min(minLength, R - L + 1) # Set the minLength to the min between current and difference between R and L pointers.
+
+        L += 1 # Increment L pointer by 1.
+
+    return 0 if minLength == float("-inf") else minLength # Return 0 if minLength has no changed from infinity.
