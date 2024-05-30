5916dd9021148e4e4a0c620c2f271bba4ad6fcb7
Roble <roblesiyad123@gmail.com>
2024-05-30 18:50:02 +0100

    The solution with explanation

--- arrays/practise/sliding-window/3_longest_substring_without_repeating_characters.py
+++ arrays/practise/sliding-window/3_longest_substring_without_repeating_characters.py
@@ -9,3 +9,31 @@ class LongestSubstringWithoutRepeatingCharacters:
     Output: 3
     Explanation: The answer is "abc", with the length of 3.
   """
+  def lengthOfLongestSubstring(s):
+    # Initialise a maxLength
+    maxLength = 0
+    # Initialise a hashMap to keep count
+    sCount = {}
+
+    # Initilise a L pointer
+    L = 0
+    # Loop through the length of the string
+    for R in range(len(s)):
+        # Add the R pointer value to the hashMap
+        sCount[s[R]] = 1 + sCount.get(s[R], 0)
+
+        # While the max(hashmap.Value()) > 1:
+        while max(sCount.values()) > 1:
+            # Remove the value at the L pointer
+            sCount[s[L]] -= 1
+            # Increment the L pointer up
+            L += 1
+
+        # Take the max between currentMax(R - L + 1) and maxLength
+        maxLength = max(R - L + 1, maxLength)
+
+    # Return the maxLength
+    return maxLength
+
+  print(lengthOfLongestSubstring('ABCABCDBABHBAB'))
+
