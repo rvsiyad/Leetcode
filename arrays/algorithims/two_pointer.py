92a78d1685fbb9b270da9165c1adbfae15d6033c
Roble <roblesiyad123@gmail.com>
2024-06-05 15:57:02 +0100

    Two pointer technique explanation and concept notes

---
+++ arrays/algorithims/two_pointer.py
@@ -0,0 +1,11 @@
+class twoPointer:
+  """
+  Two pointer is like the parent of the sliding window technique. The main idea for Two Pointer
+  is having a L (left) and R (right) pointer, both starting at some index of the array. The starting
+  points for the L and R pointer really depends on the problem at hand.
+
+  The concept:
+  - Start with a L pointer at index 0
+  - R pointer at index arr.length - 1
+  - Increment or decrement the R or L pointer, depending on the conditions for the given problem.
+  """
