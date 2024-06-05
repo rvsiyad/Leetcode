0e2fb428097f33bf479f2934bf8b8f609ea6282a
Roble <roblesiyad123@gmail.com>
2024-06-05 15:57:22 +0100

    First Two pointer example

--- arrays/algorithims/two_pointer.py
+++ arrays/algorithims/two_pointer.py
@@ -9,3 +9,20 @@ class twoPointer:
   - R pointer at index arr.length - 1
   - Increment or decrement the R or L pointer, depending on the conditions for the given problem.
   """
+
+  # Here is a simple example question:
+  # Q: Check if an array is a palindrome:
+  def isPalindrome(word):
+    # First we initialise the L and R pointer to the start and end indexes
+    L, R = 0, len(word) - 1
+
+    # create a while loop, while L pointer is less than the R pointer
+    while L < R:
+      if word[L] != word[R]: # If the values at the indexes do not match, return False
+        return False
+
+      L += 1  # Increment the L pointer up by 1
+      R -= 1 # Decrement the R pointer down by 1
+
+    return True # Return True if conditions are not met.
+
