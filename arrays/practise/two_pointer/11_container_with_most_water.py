07181e23d80ad8a67f766c561c9a27ec9d5d9278
Roble <roblesiyad123@gmail.com>
2024-06-06 22:45:58 +0100

    Solution

--- arrays/practise/two_pointer/11_container_with_most_water.py
+++ arrays/practise/two_pointer/11_container_with_most_water.py
@@ -22,3 +22,24 @@ class ContainerWithMostWater:
   Input: height = [1,1]
   Output: 1
   """
+  def maxArea(height):
+    # Holds the maxArea of water
+    maxArea = 0
+
+    L = 0 # Initialise a L pointer to the start
+    R = len(height) - 1 # Initialise R pointer to end of array
+
+    # While L pointer is less than R pointer
+    while L < R:
+        currArea = min(height[L], height[R]) * (R - L) # Calculate the area, (height x width)
+        maxArea = max(currArea, maxArea) # Set maxArea to the max between the currArea and the maxArea
+
+        if height[L] < height[R]: # If value at L is smaller than value at R, increment L pointer up and index
+            L += 1
+        elif height[R] < height[L]: # If value at R pointer is smaller than value at L, decrement the R pointer
+            R -= 1
+        else:
+            R -= 1 # If they are equal, just move one pointer
+
+    return maxArea # Return the maxArea
+
