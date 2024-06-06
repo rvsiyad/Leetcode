9010abc6581d2f3d3e04ac1569dc45490776dd93
Roble <roblesiyad123@gmail.com>
2024-06-06 20:10:51 +0100

    Solution

--- arrays/practise/two_pointer/125_valid_palindrome.py
+++ arrays/practise/two_pointer/125_valid_palindrome.py
@@ -13,3 +13,37 @@ class ValidPalindrome:
   Output: true
   Explanation: "amanaplanacanalpanama" is a palindrome.
   """
+  def isPalindrome(s):
+    # Here we create a helper function, which returns true if the a value is a valid number or letter.
+    # It works by comparing the ASCI values, and making sure the value s is between the valid character ranges.
+    def isAlphaNum(s):
+      return (ord("a") <= ord(s) <= ord("z") or
+              ord("A") <= ord(s) <= ord("Z") or
+              ord("0") <= ord(s) <= ord("9"))
+
+    # Here we create a empty string to hold our string minus the non-alphanumeric values.
+    validString = ""
+
+    # Loop through the length of s
+    for i in range(len(s)):
+        if isAlphaNum(s[i]): # If the value at s[i] is alphanumeric, it is added to the valid string
+            validString += (s[i])
+
+    # We reassign the variable to only contain lowercase letters.
+    validString = validString.lower()
+
+    # Create L and R pointer, at the start and end indexes.
+    L = 0
+    R = len(validString) - 1
+
+    # While L pointer is smaller than R pointer
+    while L < R:
+        if validString[L] != validString[R]: # If L and R pointers are not equal, can instantly return false.
+            return False
+
+        L += 1 # Increment the L pointer
+        R -= 1 # Decrement the R pointer
+
+    return True # Return true if all values at opposite ends are equal.
+
+  print(isPalindrome("A man, a plan, a canal: Panama"))
