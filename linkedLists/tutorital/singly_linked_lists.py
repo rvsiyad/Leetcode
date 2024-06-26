f669ba110b1f718c8a36c6b1b030313f50cc0326
Roble <roblesiyad123@gmail.com>
2024-06-26 13:24:31 +0100

    Operations for LinkedLists

--- linkedLists/tutorital/singly_linked_lists.py
+++ linkedLists/tutorital/singly_linked_lists.py
@@ -17,3 +17,43 @@ class SinglyLinkedList:
 
       By having the start or end nodes we can traverse the linked list.
   """
+
+  # - Creating a Linked List from scratch:
+  def __init__(self, val):
+    self.val = val # Here we are setting the current nodes value.
+    self.next = None # The next value holds the location of the next node in the linked list.
+
+  # - Setting the next node in the Linked List:
+    ListNode1.next = ListNode2  # type: ignore
+    ListNode2.next = ListNode3  # type: ignore
+    ListNode3 = None
+
+  # Traversing over a Linked List:
+  cur = ListNode1  # type: ignore
+
+  while cur: # This is able to traverse over the linked list as long as cur is not null.
+    cur = cur.next # When the cur.next is None, such as for ListNode3, the Loop will end.
+
+  # Circular Linked List are different from a normal linked list in the fact that the last node,
+  # points to the first not. So in our previous example, ListNode3 would point back to ListNode1
+
+  ListNode3.next = ListNode1  # type: ignore
+
+
+  # Operations that can be performed on a LinkedList:
+  # - Appending a new node:
+  #   This is a simple operation where the new node is set as the next node, after the tail.
+  #   And the tail is then updated to be the next node.
+
+  tail.next = ListNode4  # type: ignore
+  tail = ListNode4  # type: ignore
+
+  # - Deleting a node:
+  #   Removing a node from a LinkedList is fairly simple. It requires us to set the next pointer for the previous
+  #   node before y to the point at the node after y. The removed node does not have to be deleted as it is in memory,
+  #   and will be removed via pythons built in garbage collector.
+
+  head.next = head.next.next  # type: ignore
+
+  # This skips over the 2nd node in the Linked List, connecting ListNode1 to ListNode3.
+
