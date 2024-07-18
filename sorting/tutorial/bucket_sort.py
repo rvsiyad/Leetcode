dc59960d270a0126098b00936561b6770a9972f7
Roble <roblesiyad123@gmail.com>
2024-07-19 00:06:58 +0100

    Bucket sort code walk-through

--- sorting/tutorial/bucket_sort.py
+++ sorting/tutorial/bucket_sort.py
@@ -5,3 +5,22 @@ The idea behind it is when we have an array which has a fixed number of values (
 separate array - referred to as a bucket. After this we can loop through the bucket and add to the original array, looping through the number of
 occurrences.
 """
+
+def bucketSort(arr):
+  # Assuming we have an array which only contains a inclusive and specific number of known elements, e.g. 0,1,2
+  counts = [0,0,0]
+
+  # Count the quantity of each val in the arr
+  for n in arr:
+    counts[n] += 1
+
+  # Fill in each bucket in the original array
+  i = 0
+  for n in range(len(counts)):
+    for j in range(counts[n]):
+      arr[i] = n
+      i += 1
+
+  return arr
+
+print(bucketSort([1,2,1,2,1,2,1,2,1,1,2,0,0,0,0,0,1,2]))
