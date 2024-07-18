cc0c22710b323facad2dd5a3891b152dae9415e3
Roble <roblesiyad123@gmail.com>
2024-07-19 00:06:35 +0100

    Bucket sort introduction

---
+++ sorting/tutorial/bucket_sort.py
@@ -0,0 +1,7 @@
+"""
+Bucket sort is a unique algorithm which works much better than the other sorting algorithms in O(n) time.
+
+The idea behind it is when we have an array which has a fixed number of values (range between 0-2 etc), we can store it's occurrences in a
+separate array - referred to as a bucket. After this we can loop through the bucket and add to the original array, looping through the number of
+occurrences.
+"""
