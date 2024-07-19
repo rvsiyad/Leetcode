240d2e7500c85880ca357171e28e05134dfad2b9
Roble <roblesiyad123@gmail.com>
2024-07-19 17:05:38 +0100

    Binary search (arrays) walkthrough

--- binarySearch/tutorial/search_array.py
+++ binarySearch/tutorial/search_array.py
@@ -5,3 +5,19 @@ is higher or lower than where the middle pointer is.
 
 Binary search returns the index at which the target value is present.
 """
+def binarySearch(arr, target):
+  L, R = 0, len(arr) - 1
+
+  while L <= R:
+    m = (R + L) // 2
+
+    if target > arr[m]:
+      L = m + 1
+    elif target < arr[m]:
+      R = m - 1
+    else:
+      return m
+
+  return -1
+
+print(binarySearch([0,1,2,3,4,5,6,7,8,9,10], 8))
