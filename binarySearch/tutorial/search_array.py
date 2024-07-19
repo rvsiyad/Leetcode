ffaceebd1298d6e91eb65b9a7296e42a304b4f51
Roble <roblesiyad123@gmail.com>
2024-07-19 17:05:14 +0100

    Binary search (searching arrays) introduction

---
+++ binarySearch/tutorial/search_array.py
@@ -0,0 +1,7 @@
+"""
+Binary search is a searching algorithm that works in O(log-n) time, making it much faster than looping through an entire array.
+The algorithm only works when the array in question is sorted as it relies on splitting the array in half when the target value
+is higher or lower than where the middle pointer is.
+
+Binary search returns the index at which the target value is present.
+"""
