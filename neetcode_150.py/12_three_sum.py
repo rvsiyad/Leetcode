e54a97c57974a39b1241fdc70eae6f3109d9746c
rvsiyad <roblesiyad123@gmail.com>
2024-11-23 14:57:02 +0000

    NeetCode 12: Three Sum

---
+++ neetcode_150.py/12_three_sum.py
@@ -0,0 +1,54 @@
+"""
+Brute force solution:
+- Create a set to hold the triple values
+- Triple loop over the nums array
+- If we find three values that equal to zero,
+- Return them by adding to set as a tuple.
+- Outside loop, return using list comprehension, converting
+  tuple values into a list.
+"""
+def brute_force_solution(nums):
+  three_sums = set()
+
+  for i in range(len(nums)):
+    for j in range(i + 1,len(nums)):
+      for k in range(j + 1, len(nums)):
+        if (nums[i] + nums[j] + nums[k]) == 0:
+          three_sums.add(tuple([nums[i], nums[j], nums[k]]))
+
+  return [list(i) for i in three_sums]
+
+"""
+Optimal solution:
+- Sort the array
+- Loop over each num in nums
+- Check if current num is equal to prev num, if so, continue to next loop.
+- Do two pointer on the rest of the values
+- We can optimise by skipping values that are the same as the index prev
+- When a value is equal to 0, we need to update the pointers:
+  - We can do this by incrementing one of the pointers until they are different from the one prev.
+
+"""
+
+def optimal_solution(nums):
+  res = []
+  nums.sort()
+
+  for i in range(len(nums)):
+    L = i + 1
+    R = len(nums)
+
+    while L < R:
+      threeSum = (nums[i] + nums[L] + nums[R])
+      if threeSum > 0:
+        R -= 1
+      elif threeSum < 0:
+        L += 1
+      else:
+        res.append([nums[i], nums[L], nums[R]])
+        L += 1
+
+        while L < R and L > 0 and nums[L] == nums[L - 1]:
+          L += 1
+
+  return res
