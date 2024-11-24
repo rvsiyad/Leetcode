38e436ca0a708b8dc6df506f86d352753c9fbd55
rvsiyad <roblesiyad123@gmail.com>
2024-11-24 19:08:23 +0000

    Remove whitespace

--- neetcode_150/19_minimum_window_substring.py
+++ neetcode_150/19_minimum_window_substring.py
@@ -22,7 +22,7 @@ def optimal_solution(s, t):
 
   for c in t:
     tCount[c] = 1 + tCount.get(c, 0)
-  
+
   res, minLength, have, need = [-1, -1], float("inf"), 0, len(tCount)
 
   L = 0
@@ -32,7 +32,7 @@ def optimal_solution(s, t):
 
     if s[R] in tCount and sCount[s[R]] == tCount[s[R]]:
       have += 1
-    
+
     while have == need:
       if (R - L + 1) < minLength:
         res = [L, R]
@@ -42,9 +42,9 @@ def optimal_solution(s, t):
 
       if s[L] in tCount and sCount[s[L]] < tCount[s[L]]:
         have -= 1
-      
+
       L += 1
-  
+
   start, end = res
 
   return s[start:end+ 1] if minLength != float("inf") else ""
