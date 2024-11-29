b505d2139ef8ccd41e76d09072dc4ea8ba7935ce
rvsiyad <roblesiyad123@gmail.com>
2024-11-29 11:24:12 +0000

    Merge sort practice again

--- sorting/practise/merge_sort.py
+++ sorting/practise/merge_sort.py
@@ -42,3 +42,43 @@ def mergeSort(arr, s, e):
 arr = [5,7,8,2,3,5,79,9,2,1,3,4,4,4,5,5,5,6,67,7,7,8,8,8,8,9,5645,2,2,4234,234,23,234,23,423]
 
 print(mergeSort(arr, 0, len(arr) - 1))
+
+
+def merge_02(arr, s, m, e):
+  L = arr[s : m + 1]
+  R = arr[m + 1 : e + 1]
+
+  i = 0
+  j = 0
+  k = s
+
+  while i < len(L) and j < len(R):
+    if L[i] < R[j]:
+      arr[k] = L[i]
+      L += 1
+    else:
+      arr[k] = R[i]
+    k += 1
+
+  while i < len(L):
+    arr[k] = L[i]
+    i += 1
+    k += 1
+
+  while j < len(R):
+    arr[k] = R[j]
+    j += 1
+    k += 1
+
+def merge_sort_02(arr, s, e):
+  if (e - s + 1) <= 1:
+    return arr
+
+  m = (e - s) // 2
+
+  merge_sort_02(arr, s, m + 1)
+  merge_sort_02(arr, m + 1, e)
+
+  merge(arr, s, m, e)
+
+  return arr
