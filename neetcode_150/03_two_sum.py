c5a376f40ec5af01c88a0388e8487bd0b1dc43e3
rvsiyad <roblesiyad123@gmail.com>
2024-12-21 16:00:15 +0000

    Fix j index for two sum

--- neetcode_150/03_two_sum.py
+++ neetcode_150/03_two_sum.py
@@ -6,7 +6,7 @@ Brute force solution:
 """
 def brute_force_solution(nums, target):
   for i in range(len(nums)):
-    for j in range(i, len(nums)):
+    for j in range(i + 1, len(nums)):
       if nums[i] + nums[j] == target:
         return [i, j]
 
